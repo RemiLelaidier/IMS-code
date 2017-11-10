7915e6e15353157dc87a5bef5f79d7294144af98
Léonard Cherouvrier <leonard@bigsool.com>
2017-11-10 17:52:26 +0100

    update, deploy script pre

---
+++ assets/deploy.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+cd /var/www/clients/leonardzone/public/miageStage
+git pull origin master
+echo "done"
--- serv/app/config/config.dev.php
+++ serv/app/config/config.dev.php
@@ -5,7 +5,7 @@ use Monolog\Logger;
 $config = require __DIR__ . '/config.php';
 
 // Used for generating links in API routes markdown.
-$config['rest']['url'] = 'http://localhost/slim-rest-base';
+$config['rest']['url'] = 'http://beta.leonard.zone/';
 
 $config['monolog']['level'] = Logger::DEBUG;
 
