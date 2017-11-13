04f8eed884a32c2c971c206d509f6297423bec0b
Unknown <r.lelaidier@hotmail.fr>
2017-11-13 06:47:28 +0100

    ConventionController (Still need work)

--- serv/src/Ims/Convention/Controller/ConventionController.php
+++ serv/src/Ims/Convention/Controller/ConventionController.php
@@ -3,7 +3,10 @@
 namespace App\Ims\Convention\Controller;
 
 use App\Core\Controller\Controller;
-
+use App\Ims\Convention\Model\Convention;
+use App\Ims\Student\Model\Student;
+use App\Ims\Company\Model\Company;
+use App\Ims\Internship\Model\Internship;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -11,6 +14,8 @@ class ConventionController extends Controller
 {
 
     /**
+     * Method submit
+     * Put all the datas received from the four stages of filling the convention in the database
      * @param Request  $request
      * @param Response $response
      *
@@ -18,12 +23,11 @@ class ConventionController extends Controller
      * @throws \Psr\Container\ContainerExceptionInterface
      * @throws \Psr\Container\NotFoundExceptionInterface
      */
-    public function submit(Request $request, Response $response)
-    {
+    public function submit(Request $request, Response $response){
+        // Decode datas
         $conventionData = $request->getBody()->getContents();
-
         $decoded = json_decode($conventionData, true);
-
+        // Log activity
         $this->logger->debug('New convention on ' . get_class($this) . ":submit", [
             'data' => $conventionData
         ]);
